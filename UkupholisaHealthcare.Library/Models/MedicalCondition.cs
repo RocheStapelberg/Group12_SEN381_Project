a2c4e14c2efcd3be463ce567117074340e3189f6
Marl0u <marloufroneman@hotmail.co.za>
2022-09-28 15:17:52 +0200

    Added MedicalConditions class and propetries

---
+++ UkupholisaHealthcare.Library/Models/MedicalCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkupholisaHealthcare.Library.Models
+{
+    public class MedicalCondition
+    {
+        public int Id { get; set; }
+
+        public int FK_TreatmentId { get; set; }
+
+        public string ConditionName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
