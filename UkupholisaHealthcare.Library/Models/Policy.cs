2ed049630d4419225a3cc0ac3f3244fe8c3d5f3e
Leon Stassen <64901703+Leon-Stassen@users.noreply.github.com>
2022-09-28 15:28:52 +0200

    added policy class with properties

---
+++ UkupholisaHealthcare.Library/Models/Policy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkupholisaHealthcare.Library.Models
+{
+    internal class Policy
+    {
+        public int Id { get; set; }
+        public int FK_FamilyLink_Id { get; set; }
+        public string? PolicyName { get; set; }
+        public string? Description { get; set; }
+        public decimal PolicyPrice { get; set; }
+        public bool? IsApproved { get; set; }
+    }
+}
