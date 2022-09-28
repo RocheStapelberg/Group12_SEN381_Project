08b97d6408e43c1de5fb99269df6b8f6b1410943
Marl0u <marloufroneman@hotmail.co.za>
2022-09-28 15:11:09 +0200

    Provider added properties

---
+++ UkupholisaHealthcare.Library/Models/Provider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkupholisaHealthcare.Library.Models
+{
+     public class Provider
+    {
+        public int Id { get; set; }
+        public string ProviderName { get; set; }
+        public string Adress { get; set; }
+
+        public string Contact { get; set; }
+    }
+}
