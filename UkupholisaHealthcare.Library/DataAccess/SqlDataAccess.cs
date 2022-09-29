d6d3e99d8cfc2bc24adc2a9594f9a65f840d7f2d
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-09-29 13:39:08 +0200

    Implemented main data access class

--- UkupholisaHealthcare.Library/DataAccess/SqlDataAccess.cs
+++ UkupholisaHealthcare.Library/DataAccess/SqlDataAccess.cs
@@ -1,26 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace UkupholisaHealthcare.Library.DataAccess
 {
-    public class SqlDataAccess
+    /// <summary>
+    /// Main DataAccess Class
+    /// </summary>
+    public class SqlDataAccess : IDisposable
     {
-        public void GetConnectionstring()
+
+        // Open connection/start transaction
+        // Load using the transaction
+        // Save using transaction
+        // Close connection/stop transaction
+        // Dispose
+
+        public SqlDataAccess(IConfiguration config, ILogger<SqlDataAccess> logger)
         {
+            _config = config;
+            _logger = logger;
+        }
+
+        public string GetConnectionString(string name)
+        {
+            return _config.GetConnectionString(name);
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                List<T> rows = cnn.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+        private readonly IConfiguration _config;
+        private readonly ILogger<SqlDataAccess> _logger;
+
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+
+            _transaction = _connection.BeginTransaction();
+        }
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+            List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+            return rows;
+        }
+
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            _connection.Execute(storedProcedure, parameters,
+                commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _transaction = null;
+
+            _connection?.Close();
+            _connection = null;
 
         }
 
-        public void LoadData()
+        public void RollbackTransaction()
         {
+            _transaction?.Rollback();
+            _transaction = null;
+
+            _connection?.Close();
+            _connection = null;
 
         }
 
-        public void SaveData()
+        public void Dispose()
         {
-
+            try
+            {
+                CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Commit Transaction failed in dispose method.");
+            }
         }
 
     }
