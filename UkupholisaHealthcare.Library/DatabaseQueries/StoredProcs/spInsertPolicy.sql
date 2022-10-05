494eeeecfc2d73644f725290332092babfd04f31
Marl0u <marloufroneman@hotmail.co.za>
2022-10-05 16:35:07 +0200

    Created Insert Storet Procedure for Policy table

---
+++ UkupholisaHealthcare.Library/DatabaseQueries/StoredProcs/spInsertPolicy.sql
@@ -0,0 +1,20 @@
+USE UkupholisaDb
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE spInsertPolicy
+    @Family_Id  INT,
+	@PolicyName NVARCHAR(50), 
+	@Description NVARCHAR(max), 
+	@PolicyPrice money,
+	@isApproved bit
+AS
+BEGIN
+	SET NOCOUNT ON;
+    -- Insert SP for Provider
+	INSERT INTO [Policy](Family_id,PolicyeName, [Description] ,PolicyPrice,isApproved)
+	VALUES (@Family_Id,@PolicyName, @Description, @PolicyPrice,@isApproved);
+END
+GO
