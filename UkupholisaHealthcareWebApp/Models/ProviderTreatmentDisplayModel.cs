be748fdaac820ed867f576bf15ddcb3bc8d09510
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-10-17 09:19:33 +0200

    Added needed Views

---
+++ UkupholisaHealthcareWebApp/Models/ProviderTreatmentDisplayModel.cs
@@ -0,0 +1,18 @@
+﻿using UkupholisaHealthcare.Library.Models;
+
+namespace UkupholisaHealthcareWebApp.Models
+{
+    public class ProviderTreatmentDisplayModel
+    {
+        public ProviderTreatmentDisplayModel(Provider provider, List<Treatment> treatments)
+        {
+            this.provider = provider;
+            this.treatments = treatments;
+        }
+
+        public Provider provider { get; set; }
+        public List<Treatment> treatments { get; set; }
+
+
+    }
+}
--- UkupholisaHealthcareWebApp/Program.cs
+++ UkupholisaHealthcareWebApp/Program.cs
@@ -16,6 +16,7 @@ namespace UkupholisaHealthcareWebApp
             builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
             builder.Services.AddTransient<IUserData, UserData>();
             builder.Services.AddTransient<IProviderData, ProviderData>();
+            builder.Services.AddTransient<ITreatmentData, TreatmentData>();
 
             var app = builder.Build();
 
