5621d08a4743ab67c8659e36bf30626fd40b5917
Marl0u <marloufroneman@hotmail.co.za>
2022-10-17 14:32:13 +0200

    Added MedicalCondition DataClass and Interface

---
+++ UkupholisaHealthcare.Library/Data/IMedicalCondition.cs
@@ -0,0 +1,10 @@
+﻿namespace UkupholisaHealthcare.Library.Data
+{
+    public interface IMedicalCondition
+    {
+        void DeleteMedicalCondition(global::System.Int32 id);
+        List<MedicalCondition> GetAllMedicalCondition();
+        void InsertMedicalCondition(MedicalCondition medicalCondition);
+        void UpdateMedicalCondition(MedicalCondition medicalCondition);
+    }
+}
---
+++ UkupholisaHealthcare.Library/Data/MedicalCondition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UkupholisaHealthcare.Library.DataAccess;
+using UkupholisaHealthcare.Library.Models;
+
+namespace UkupholisaHealthcare.Library.Data
+{
+    public class MedicalCondition : IMedicalCondition
+    {
+        private readonly ISqlDataAccess _sql;
+
+        public MedicalConditionData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+
+        public List<MedicalCondition> GetAllMedicalCondition()
+        {
+            var output = _sql.LoadData<MedicalCondition, dynamic>("spGetAllMedicalCondition", new { });
+            return output;
+
+        }
+        public void InsertMedicalCondition(MedicalCondition medicalCondition)
+        {
+            _sql.RunStoredProcedure("spInsertMedicalCondition", medicalCondition);
+        }
+
+        public void UpdateMedicalCondition(MedicalCondition medicalCondition)
+        {
+            _sql.RunStoredProcedure("spUpdateMedicalCondition", medicalCondition);
+        }
+
+        public void DeleteMedicalCondition(int id)
+        {
+            _sql.RunStoredProcedure("spDeleteMedicalCondition", new { id });
+        }
+
+    }
+}
